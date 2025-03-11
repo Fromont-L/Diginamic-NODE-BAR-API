20ed394ec298ee6eeb467fc2d26b87b6b69c6169
Fromont-L <lucasfromont97@gmail.com>
2025-03-11 10:47:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// INDEX
+const express = require("express");
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Tourne sur le port : ", PORT);
+});
