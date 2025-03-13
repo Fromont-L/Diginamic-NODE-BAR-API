5d02bfcb14dba1a86bd0106b66de9baad652f28f
Pic Aurélia <pic.aurelia@gmail.com>
2025-03-13 18:06:55 +0100

    Composant Button done

---
+++ client/src/components/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let text = 'Cliquez ici';
+    export let color = 'bg-[var(--clr-yellow)]'; 
+    export let textColor = 'text-black'; 
+    export let onClick = () => {};
+  </script>
+  
+  <button
+    class={`w-xs h-14 rounded ${textColor} ${color} text-2xl`}
+    on:click={onClick}
+  >
+    {text}
+  </button>
+  
