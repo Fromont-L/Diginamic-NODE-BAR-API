3743884b8be2bebc1f1b0463cb00c037c62a6f51
Fromont-L <lucasfromont97@gmail.com>
2025-03-17 12:32:36 +0100

    Visuel modal "Ajouter un bar"

--- client/src/components/BarModal.svelte
+++ client/src/components/BarModal.svelte
@@ -1,4 +1,3 @@
-<!-- BarModal.svelte - Composant à créer pour réutiliser dans les deux pages -->
 <script>
     import Modal from './BiereCommandeModale.svelte';
     import { createEventDispatcher } from 'svelte';
@@ -34,43 +33,33 @@
     on:close={handleClose}
     on:submit={handleSubmit}
   >
-    <div slot="content">
-      <div class="form-group">
-        <label for="name">Nom</label>
-        <input id="name" name="name" type="text" bind:value={currentBar.name} class="w-full p-2 border rounded" />
+    <div slot="content" class="flex flex-col gap-4">
+      <div class="mb-4 flex items-center justify-end gap-12">
+        <label for="name" class="block mb-2 font-medium text-left">Nom</label>
+        <input id="name" name="name" type="text" bind:value={currentBar.name} class="w-4/6 p-2 bg-[var(--clr-white)] rounded text-black" />
       </div>
       
-      <div class="form-group">
-        <label for="adresse">Adresse</label>
-        <input id="adresse" name="adresse" type="text" bind:value={currentBar.adresse} class="w-full p-2 border rounded" />
+      <div class="mb-4 flex items-center justify-end gap-12">
+        <label for="adresse" class="block mb-2 font-medium text-left">Adresse</label>
+        <input id="adresse" name="adresse" type="text" bind:value={currentBar.adresse} class="w-4/6 p-2 bg-[var(--clr-white)] rounded text-black" />
       </div>
       
-      <div class="form-group">
-        <label for="tel">Téléphone</label>
-        <input id="tel" name="tel" type="tel" bind:value={currentBar.tel} class="w-full p-2 border rounded" />
+      <div class="mb-4 flex items-center justify-end gap-12">
+        <label for="tel" class="block mb-2 font-medium text-left">Téléphone</label>
+        <input id="tel" name="tel" type="tel" bind:value={currentBar.tel} class="w-4/6 p-2 bg-[var(--clr-white)] rounded text-black" />
       </div>
       
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input id="email" name="email" type="email" bind:value={currentBar.email} class="w-full p-2 border rounded" />
+      <div class="mb-4 flex items-center justify-end gap-12">
+        <label for="email" class="block mb-2 font-medium text-left">Email</label>
+        <input id="email" name="email" type="email" bind:value={currentBar.email} class="w-4/6 p-2 bg-[var(--clr-white)] rounded text-black" />
       </div>
       
-      <div class="form-group">
-        <label for="description">Description</label>
-        <textarea id="description" name="description" bind:value={currentBar.description} class="w-full p-2 border rounded h-24"></textarea>
+      <div class="mb-4 flex items-center justify-end gap-12">
+        <label for="description" class="block mb-2 font-medium text-left">Description</label>
+        <textarea id="description" name="description" bind:value={currentBar.description} class="w-4/6 p-2 bg-[var(--clr-white)] rounded text-black h-24"></textarea>
       </div>
     </div>
-  </Modal>
-  
-  <style>
-    .form-group {
-      margin-bottom: 15px;
-    }
     
-    label {
-      display: block;
-      margin-bottom: 5px;
-      font-weight: bold;
-    }
-  </style>
+  </Modal>
+
   
